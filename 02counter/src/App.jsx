c9543ea5066fa2aa6712f36a5997c42519f530b8
Anurag Gupta <anurag6967@gmail.com>
2024-03-14 17:10:36 +0530

    assignment done to stop at 20 and 0 when we add and remove respectively

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -15,16 +15,39 @@ function App() {
   function remove(){
     setCounter(counter-1)
   }
+
+  //assignment to control the counter value between (0-20)......................
+  //add value but not go above 20
+  function addA(){
+    if(counter === 20){
+      alert('reached at highest value')
+      return;
+    }
+    else{
+      setCounter(counter+1);
+    }
+  }
  
+  //remove value but go below 0
+  function removeA(){
+    if(counter === 0){
+      alert('reached at lowest value')
+      return;
+    }
+    else{
+      setCounter(counter-1)
+    }
+  }
+
 
   return (
     <>
       <h1>Chai Aur Code Vite</h1>
       <h1>Counter Value:{counter}</h1>
 
-      <button onClick={add}> Add Value {counter} </button>  
+      <button onClick={addA}> Add Value {counter} </button>  
       <br />
-      <button onClick={remove}> Remove Value{counter} </button>  
+      <button onClick={removeA}> Remove Value{counter} </button>  
      
     </>
   )
