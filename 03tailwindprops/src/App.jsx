147e91764016a583289a193e77a217099a9092a9
Anurag Gupta <anurag6967@gmail.com>
2024-03-15 17:35:44 +0530

    props passing propeties to component and and using that properties in component

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+import ArrayCard from './components/ArrayCard'
 
-function App() {
+function App(props) {
   const [count, setCount] = useState(0)
+
+  const obj={
+    name:"React Series Object",
+    age:25,
+    email:'anurag@gmail.com',
+    profile:"React JS"
+  }
+  const obj2={
+    name:"Javacript Series Object",
+    age:30,
+    email:'anurag@gmail.com',
+    profile:"Javascript"
+  }
+
+  let arr=[1,2,3,3000];
+  // <Card arr={arr} />
+  
 
   return (
     <>
-    <h1 className='bg-green-400 text'> Tailwind Config in React Project by vite </h1>
+    <h1 className='bg-green-400 text-black p-4 rounded-2xl boder-2 mb-5'> Tailwind Config in React Project by vite {props.name} </h1>
+
+   <Card username="Js" age={10} /> 
+   <Card name="React JS" a={20} /> 
+   <Card n="Node Js" ag={30}    /> 
+   <Card o={obj}    /> 
+   <Card o={obj2}    /> 
+   <Card o={obj2}    /> 
+   <Card o={obj}    /> 
+    
+  <ArrayCard a={arr} />
+   
+   
+   
+
+ 
     </>
   )
 }
--- 03tailwindprops/src/main.jsx
+++ 03tailwindprops/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+let array=[10,20,30,40]
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+ 
+    <App  name={array} />
+    
   </React.StrictMode>,
 )
