aa120799f099793e0d1dc7a7da5601d2d59974e3
Anurag Gupta <anurag6967@gmail.com>
2024-03-22 17:32:05 +0530

    completed currency converter project with some extra feature like select currency

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,4 +1,16 @@
-import {useState,useEffect} from 'react'
+import { useEffect, useState } from "react"
 
 
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-21/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        // console.log('data is ',data);
+    }, [currency])
+    // console.log(data);
+    return data
+}
 
+export default useCurrencyInfo;
--- currency.md
+++ currency.md
@@ -3,7 +3,10 @@
 ## api link
 
 ```javascript
-let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+
+let url= `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-12/v1/currencies/${currency}usd.json`
+
+let usrl2=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-12/v1/currencies/usd.json`
 
 ```
 
