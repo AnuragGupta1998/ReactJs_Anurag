ce4542c27fa09d756ea9bf9999c672d51c547d1e
Anurag Gupta <anurag6967@gmail.com>
2024-04-01 14:24:11 +0530

    crating context provider that wrap the components and give them access to globle variables

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -1,1 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider=({children})=>{
+   
+    //global variable that can access by any children
+    const[user,setUser]=useState(null)
+
+    return(
+     
+        < UserContext.Provider value={{user,setUser}}>
+
+         {children}
+
+        </ UserContext.Provider >
+
+    )
+
+}
+
+
+
+
+export default UserContextProvider
