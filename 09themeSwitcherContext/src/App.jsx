a67c0b499ae498844c95a21d0cafad870413e6b4
Anurag Gupta <anurag6967@gmail.com>
2024-04-03 21:34:35 +0530

    added theme component and card component into app.jsx

--- 09themeSwitcherContext/src/App.jsx
+++ 09themeSwitcherContext/src/App.jsx
@@ -1,37 +1,48 @@
 
-import {useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { ThemeContextProvider } from './contexts/theme'
+import ThemeButton from './components/ThemeButton';
+import Card from './components/Card'
 
 
 
 function App() {
 
-  const[themeMode,setThemeMode]=useState('light');
- 
-  //defing function here that can change function inside ThemeContext too..........
-    const lightTheme=()=>{
-      setThemeMode("light") 
-    }
-    const darkTheme=()=>{
-      setThemeMode("dark")
-    }
+  const [themeMode, setThemeMode] = useState('light');
 
-    //actual changing the Theme......
-  
+  //defing function here that can change function inside ThemeContext too..........
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  //actual changing the Theme......
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+
+
+    document.querySelector('html').classList.add(themeMode)
+
+  },[themeMode])
+
   return (
-    <ThemeContextProvider value={{themeMode,lightTheme,darkTheme}}>
+    <ThemeContextProvider value={{ themeMode, lightTheme, darkTheme }}>
 
       <div className="flex flex-wrap min-h-screen items-center">
 
         <div className="w-full">
 
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* theme button */}
+            <ThemeButton 
+              value={themeMode}
+            />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-            {/* Cards */}
+            <Card />
           </div>
 
         </div>
