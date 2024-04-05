d436652e9f2789ae847f3750279c258d32261991
Anurag Gupta <anurag6967@gmail.com>
2024-04-05 20:49:03 +0530

    writing method for todo and local storage to store todos

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  //add todo
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), todo }, ...prev])
+  }
+
+  //update todo
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+  }
+
+  //delete todo
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id))
+  }
+
+  // toggle OR checked todo
+  const toggleComplete = (id) => {
+
+    //overiding completed value
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
+  }
+
+  //getting todos from local storage
+  useEffect(()=>{
+    //local storege gives result in string then convert into json
+    const todos=JSON.parse(localStorage.getItem("todos"))
+
+    if(todos){
+      setTodos(todos)
+    }
+  },[])
 
   return (
-    <>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete, }}>
+
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -19,7 +53,8 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+
+    </TodoProvider>
   )
 }
 
