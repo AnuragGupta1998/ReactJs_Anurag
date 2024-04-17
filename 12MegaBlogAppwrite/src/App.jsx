8036d87a39ed7920c3e1b53206b19f3848abb0f4
Anurag Gupta <anurag6967@gmail.com>
2024-04-17 23:50:38 +0530

    accessing .env variables in app.jsx is successfully

--- 12MegaBlogAppwrite/src/App.jsx
+++ 12MegaBlogAppwrite/src/App.jsx
@@ -1,9 +1,15 @@
 
 import './App.css'
+import {conf} from './conf/conf'
 
 function App() {
+  //accessing .enc file variable.................
   
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(conf.appwriteDatabaseId)
+  console.log(conf.appwriteUrl)
+
+
 
   return (
     <>
