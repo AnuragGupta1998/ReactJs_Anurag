42fbf053294eb102049ebb1b478bc0b11178b561
Anurag Gupta <anurag6967@gmail.com>
2024-04-17 23:49:22 +0530

    create folder in which accessing .env variables in conf.js as key value pair and exported

---
+++ 12MegaBlogAppwrite/src/conf/conf.js
@@ -0,0 +1,13 @@
+export const conf={
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+
+    appwriteProjectIdl:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATEBASE_ID),
+
+    appwriteCoolectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
