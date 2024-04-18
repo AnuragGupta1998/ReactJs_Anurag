0a73a80bf5a5ce77808c9bd877cdc1d306c6fcda
Anurag Gupta <anurag6967@gmail.com>
2024-04-18 11:01:43 +0530

    debugging in conf.js error in naming of variables of .env variables

--- 12MegaBlogAppwrite/src/conf/conf.js
+++ 12MegaBlogAppwrite/src/conf/conf.js
@@ -1,13 +1,15 @@
-export const conf={
-    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+ const conf={
 
-    appwriteProjectIdl:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
 
-    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATEBASE_ID),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
 
-    appwriteCoolectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 
-    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
 }
 
 export default conf
