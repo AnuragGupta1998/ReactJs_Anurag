9f8c12a9988d841ce1040c5b0f0a86d3e8e99d79
Anurag Gupta <anurag6967@gmail.com>
2024-04-19 11:10:03 +0530

    signup,login,logout,getUser completed with appwrite auth service

--- 12MegaBlogAppwrite/src/appwrite/auth.js
+++ 12MegaBlogAppwrite/src/appwrite/auth.js
@@ -2,61 +2,75 @@ import conf from '../conf/conf.js'
 import { Client, Account, ID } from "appwrite";
 
 
-export class AuthService{
+export class AuthService {
 
-    client=new Client();
+    client = new Client();
     account;
-    constructor(){
-        //setting client and account.....
+    constructor() {
+        //setting up client and account.....
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
 
-        this.account=new Account(this.client);
+        this.account = new Account(this.client);
     }
 
-    //creating account.......
-    async creatAccount({email,password,name}){
+    //creating account by appwrite createAccount method .............................
+    //Signup
+    async creatAccount({ email, password, name }) {
 
         try {
 
-           const userAccount=await this.account.create(ID.unique(),email,password,name);
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
 
-           if(userAccount){
-            //calling login method "if userAccount created successfullythen login the user into his account"
-            return this.login({email,password})
-           }
-           else {
-              return userAccount;
+            //if userAccount created then login the user
+            if (userAccount) {
+
+                //calling login method "if userAccount created successfullythen login the user into his account"
+                return this.login({ email, password })
             }
-
-            
+            else {
+                return userAccount;
+            }
         }
         catch (error) {
-            throw error   
+            throw error
         }
-
-
-
+    }
+    //login the user if they successfukky created theit account....
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        }
+        catch (error) {
+            console.log("appwrite service error in login", error)
+        }
     }
 
+    //getCurrentUser who is logined......
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
 
+        } catch (error) {
+            console.log("Appwrite error in getCurrentUser", error)
+        }
+        return null;
+    }
 
+    //logout the user.........
+    async logout() {
+        try {
+            await this.account.deleteSessions()
 
+        } catch (error) {
+            console.log('appwrite service :: logout :: error', error)
 
-
-
-
-
-
-
-
-
+        }
+    }
 }
 
-
-
 //creating object of class...........
-const authService=new AuthService()
+const authService = new AuthService()
 
 export default authService;
